70ea473ed62797a95bfe62c317aeee8c276ddfe6
OhayoNB <ohayonzzb@gmail.com>
2020-09-30 18:55:00 +0300

    test

--- CouponSpringProject/src/main/java/com/model/ClientType.java
+++
@@ -1,9 +0,0 @@
-package com.model;
-
-public enum ClientType {
-
-	//Client types
-	Administrator,
-	Company,
-	Customer
-}
--- CouponSystemWebsite/src/app/models/clientType.model.ts
+++
@@ -1,7 +0,0 @@
-export enum ClientType {
-
-	//Client types
-	Administrator,
-	Company,
-	Customer
-}
